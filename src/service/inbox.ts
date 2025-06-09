6eac381bc10d60749ceafe9d1ab310209729f07c
Wahyu Ramadan <wramadan1203@gmail.com>
2025-06-09 22:25:33 +0700

    fix: use console to define unused var

--- src/service/inbox.ts
+++ src/service/inbox.ts
@@ -13,6 +13,7 @@ export const getInboxs = async (search?: string) => {
 };
 
 export const getInbox = async (idInbox: string) => {
+  console.log("Trigger getInbox with ID: ", idInbox);
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/posts/1"
   );
@@ -24,6 +25,8 @@ export const patchInbox = async (
   idInbox: string,
   payload: Partial<InboxType>
 ) => {
+  console.log("Trigger patchInbox with ID: ", idInbox);
+
   const response = await axios.patch(
     "https://jsonplaceholder.typicode.com/posts/1",
     payload
@@ -42,6 +45,7 @@ export const postChat = async (payload: ChatType) => {
 };
 
 export const getChats = async (idInbox: string) => {
+  console.log("Trigger getChats with ID: ", idInbox);
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/comments/1"
   );
@@ -50,6 +54,7 @@ export const getChats = async (idInbox: string) => {
 };
 
 export const getChat = async (idChat: string) => {
+  console.log("Trigger getChat with ID: ", idChat);
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/comments/1"
   );
@@ -58,6 +63,7 @@ export const getChat = async (idChat: string) => {
 };
 
 export const patchChat = async (idChat: string, payload: Partial<ChatType>) => {
+  console.log("Trigger patchChat with ID: ", idChat);
   const response = await axios.patch(
     "https://jsonplaceholder.typicode.com/comments/1",
     payload
@@ -67,6 +73,7 @@ export const patchChat = async (idChat: string, payload: Partial<ChatType>) => {
 };
 
 export const deleteChat = async (idChat: string) => {
+  console.log("Trigger deleteChat with ID: ", idChat);
   const response = await axios.delete(
     "https://jsonplaceholder.typicode.com/comments/1"
   );
